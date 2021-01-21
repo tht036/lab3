bf9c266ca3359e2efc5b9608d199fa4320d5cd4a
Hannah Truong <tht036@ucsd.edu>
2021-01-21 10:57:58 -0800

    msg

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
@@ -10,9 +11,28 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("La la la");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e) { 
+	console.log("Project Clicked")
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+        $(description).fadeOut();
+    }
+}
+
